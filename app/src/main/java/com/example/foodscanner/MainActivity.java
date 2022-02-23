ff47bee678f303c77a85e44f8e3a3d695152da23
aksha <akshaykokadwar123@gmail.com>
2022-02-23 22:41:34 +0530

    git@github.com:KOKADWARAkshay/FoodScanner.git

---
+++ app/src/main/java/com/example/foodscanner/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.foodscanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);    //turn off night mode
+    }
+
+}
